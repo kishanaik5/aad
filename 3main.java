58c8b8c110a390441f028decdf69fa176ab765a8
Kishan K <120300882+kishanaik5@users.noreply.github.com>
2024-01-08 23:03:10 +0530

    Update 3main.java

--- 3main.java
+++ 3main.java
@@ -7,8 +7,7 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import
-com.example.myapplication.databinding.ActivityMainBinding;
+import com.example.myapplication.databinding.ActivityMainBinding;
 import android.view.Menu;
 import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
